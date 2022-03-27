7e69cb38cf6db1b3ebf8cafa30b4b227e0ef066a
hobart cai <hobart.cai@outlook.com>
2022-03-27 23:41:04 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:HobartCai/vue-canvas-ruler.github.io.git main:gh-pages
+git push -f https://github.com/HobartCai/vue-canvas-ruler.git main:gh-pages
 
 cd -
